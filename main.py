97097039d6b6f253f99ded9a868f44cac68ae37f
LeakedBuffalo7907 <69818555+LeakedBuffalo7907@users.noreply.github.com>
2021-01-26 20:16:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ from colorama import Fore
 
 scrape = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Proxy Scrape? {Fore.WHITE}(yes or no){Fore.LIGHTBLACK_EX}: {Fore.WHITE}")
 
+def scrape():
+    scraped = 0
+    f = open("proxies.txt", "a+")
+    f.truncate(0)
+    r = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1500&ssl=yes')
+    proxies = []
+    for proxy in r.text.split('\n'):
+        proxy = proxy.strip()
+        if proxy:
+            proxies.append(proxy)
+    for p in proxies:
+        scraped = scraped + 1 
+        f.write((p)+"\n")
+    f.close()
+    print(f"{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Scraped {Fore.WHITE}{scraped} {Fore.LIGHTBLACK_EX}proxies.")
+    print(f"{Fore.WHITE}Press enter to quit")
+    input()
+    
 if scrape == "no":
+  print(f"{Fore.WHITE}Press enter to quit")
   input()
 
 if scrape == "yes":
